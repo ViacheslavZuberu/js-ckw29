58aaa834c154d932bd055a9b2b69da3928472ffb
Viacheslav <viacheslav.sheviakov@proarea.co>
2021-01-13 20:19:56 +0200

    [Lesson] Update

--- 130121_arrays/1_example/js/main.js
+++ 130121_arrays/1_example/js/main.js
@@ -17,3 +17,39 @@ let sparseArray = [,,,,,,5];
 console.log(names.length);
 console.log(sparseArray);
 
+numbers[6] = 3.1415;
+
+console.log(numbers);
+
+
+// 1) Вывести массив
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// 2) Вывоести только четные элементы
+for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+
+// 3) Сумма элементов
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log(sum);
+
+// 4) Максимальный элемент
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (max < numbers[i]) {
+        max = numbers[i];
+    }
+}
+
+console.log(max);
